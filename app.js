df48128dda1eb794f2a179f7ecbe631ca5bec3b8
shashikanthvnmmattapalli <45655351+shashikanthvnmmattapalli@users.noreply.github.com>
2024-04-19 22:48:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 8080;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud');
+  res.send('Hello Vijaykanth Welcome to world');
 })
 
 app.listen(port, host);
